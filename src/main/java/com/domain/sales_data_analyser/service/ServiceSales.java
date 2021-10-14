5dcbcbfe5c7f35ab3454e0035ae5f25d4dee08f0
Bruno Almeida Rabelo <barabelo@hotmail.com>
2021-10-14 18:46:02 -0300

    Refatoração

--- src/main/java/com/domain/sales_data_analyser/service/ServiceSales.java
+++ src/main/java/com/domain/sales_data_analyser/service/ServiceSales.java
@@ -3,20 +3,28 @@ package com.domain.sales_data_analyser.service;
 import com.domain.sales_data_analyser.model.Sale;
 import com.domain.sales_data_analyser.threads.SearchByRegionWorker;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class ServiceSales {
+    private static final int numWorkers = 10;
+    private static ConcurrentLinkedDeque<Sale> searchResult;
+    private static Thread[] threads;
+    private static int fromIndex;
+    private static int salesPerWorker;
+    private static int toIndex;
+
+    private static void initialize(int salesListSize) {
+        searchResult = new ConcurrentLinkedDeque<>();
+        threads = new Thread[numWorkers];
+        fromIndex = 0;
+        salesPerWorker = salesListSize / numWorkers;
+        toIndex = fromIndex + salesPerWorker + salesListSize % numWorkers;
+    }
+
     public static ConcurrentLinkedDeque<Sale> searchByRegion(List<Sale> salesList, String key) {
-        final int numWorkers = 10;
-
-        ConcurrentLinkedDeque<Sale> searchResult = new ConcurrentLinkedDeque<>();
-        Thread[] threads = new Thread[numWorkers];
-
-        int fromIndex = 0;
-        int salesPerWorker = salesList.size() / numWorkers;
-        int toIndex = fromIndex + salesPerWorker + salesList.size() % numWorkers;
-
+        initialize(salesList.size());
         for (int i = 0; i < threads.length; i++) {
             SearchByRegionWorker searchByRegionWorker
                     = new SearchByRegionWorker(salesList.subList(fromIndex, toIndex), searchResult, key);
@@ -25,14 +33,13 @@ public class ServiceSales {
             fromIndex = toIndex;
             toIndex = fromIndex + salesPerWorker;
         }
-
-        for (int i = 0; i < threads.length; i++) {
+        for (Thread thread : threads)
             try {
-                threads[i].join();
+                thread.join();
             } catch (InterruptedException e) {
                 throw new RuntimeException();
             }
-        }
         return searchResult;
     }
+
 }
